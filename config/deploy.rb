37d67e96906d5eb01545ab363a1479b5e73497e8
S-C-miyamoto <raisbritannia2020@gmail.com>
2020-01-16 14:46:42 +0900

    自動デプロイ　その２

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, ''
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:S-C-miyamoto/chat-space.git'
@@ -32,3 +32,4 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+
